2d4b58b7ecea93aaef55380fb5c1e7af4b6a343b
bayulaxana <bynd4spd@gmail.com>
2020-11-26 14:24:42 +0700

    added Contact DB Helper and Contact class for DB models :)

--- app/src/main/java/com/example/contactsmobile/MainActivity.java
+++ app/src/main/java/com/example/contactsmobile/MainActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity
 {
+    ContactDbHelper dbHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dbHelper = new ContactDbHelper(this);
     }
 }
