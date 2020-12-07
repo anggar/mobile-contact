f67872ec521be597bc7e58b4c8ff1399fe259ae7
anggar <anggarwahyu@gmail.com>
2020-12-07 09:02:28 +0700

    Blatant revamp for task

--- app/src/main/java/com/example/contactsmobile/ContactFormActivity.java
+++ app/src/main/java/com/example/contactsmobile/ContactFormActivity.java
@@ -3,41 +3,30 @@ package com.example.contactsmobile;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.location.Address;
-import android.location.Geocoder;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.text.Editable;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.button.MaterialButton;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.material.textfield.TextInputLayout;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
 
 public class ContactFormActivity extends AppCompatActivity
-    implements OnMapReadyCallback, GoogleMap.OnMapClickListener
 {
     private GoogleMap mGmap;
     private Marker mapMarker;
@@ -48,39 +37,7 @@ public class ContactFormActivity extends AppCompatActivity
 
     private static final double DEFAULT_LAT = 0.7893;
     private static final double DEFAULT_LNG = 113.9213;
-    private Button btnAdd;
     private Uri uriPhoto;
-
-    private Contact getContact() {
-        int[] tetViewId = { R.id.tetName, R.id.tetPhone, R.id.tetAddress };
-        ArrayList<String> formValues = new ArrayList<>();
-
-        for (int viewId: tetViewId) {
-            Editable et = ((TextInputEditText) findViewById(viewId)).getText();
-            if (et == null) return null;
-
-            String value = et.toString();
-
-            if (value.isEmpty()) {
-                Toast.makeText(this, "Insufficient data", Toast.LENGTH_SHORT).show();
-                return null;
-            } else {
-                formValues.add(et.toString());
-            }
-        }
-
-        String latitude = "NaN", longitude = "Nan";
-        if (mapLatLng != null) {
-            latitude = String.valueOf(mapLatLng.latitude);
-            longitude = String.valueOf(mapLatLng.longitude);
-        }
-
-        return new Contact(
-                0, // unset
-                formValues.get(0), formValues.get(1), formValues.get(2),
-                latitude, longitude, uriPhoto != null ? uriPhoto.toString() : null
-        );
-    }
 
     private void pickPhoto() {
         Intent picker = new Intent(Intent.ACTION_GET_CONTENT);
@@ -141,37 +98,6 @@ public class ContactFormActivity extends AppCompatActivity
         super.onActivityResult(requestCode, resultCode, data);
     }
 
-    private void searchLocation() {
-        Geocoder geocoder = new Geocoder(ContactFormActivity.this);
-        Editable et = ((TextInputEditText) findViewById(R.id.tetAddress)).getText();
-        List<Address> addrResult;
-
-        if (et != null && et.toString().isEmpty()) {
-            Toast.makeText(ContactFormActivity.this, "Provide a location",
-                    Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        try {
-            assert et != null;
-            addrResult = geocoder.getFromLocationName(et.toString(), 1);
-
-            if (addrResult.isEmpty()) {
-                Toast.makeText(ContactFormActivity.this, "Location not found",
-                        Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-            Address address = addrResult.get(0);
-            Double lat = address.getLatitude(),
-                   lng = address.getLongitude();
-            setPointOnMap(lat, lng);
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     private void onEditIntent() {
         ActionBar actionBar = getSupportActionBar();
 
@@ -179,16 +105,7 @@ public class ContactFormActivity extends AppCompatActivity
             actionBar.setTitle("Edit contact");
         }
 
-        btnAdd.setText("Save contact");
-
-        TextInputEditText tetName = findViewById(R.id.tetName);
-        TextInputEditText tetPhone = findViewById(R.id.tetPhone);
-        TextInputEditText tetAddress = findViewById(R.id.tetAddress);
         ImageView ivPhoto = findViewById(R.id.ivPhoto);
-
-        tetName.setText(contact.getName());
-        tetPhone.setText(contact.getPhone());
-        tetAddress.setText(contact.getAddress());
 
         String photo = contact.getPhoto();
         if (photo != null) {
@@ -196,98 +113,21 @@ public class ContactFormActivity extends AppCompatActivity
         }
     }
 
-    private void onEditSave() {
-        Contact contact = getContact();
-        Intent intent = new Intent();
-
-        ContactDbHelper.updateOne(contactId, contact);
-        setResult(2, intent);
-        finish();
-    }
-
-    private void onAddContact() {
-        Contact contact = getContact();
-        Intent intent = new Intent();
-
-        if (contact != null) {
-            ContactDbHelper.insertOne(contact);
-            setResult(1, intent);
-            finish();
-        }
-    }
-
-    private void setPointOnMap(Double lat, Double lng) {
-        LatLng latLng = new LatLng(lat, lng);
-
-        if (mapMarker != null) mapMarker.remove();
-
-        MarkerOptions markerOpt = new MarkerOptions()
-                .position(latLng)
-                .title("Selected Location");
-        mGmap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
-
-        mapMarker = mGmap.addMarker(markerOpt);
-        mapLatLng = latLng;
-    }
-
-    private void initMap() {
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        assert mapFragment != null;
-        mapFragment.getMapAsync(this);
-    }
-
-    private boolean locationProvided() {
-        return (!contact.getLatitude().equals("Nan") &&
-                !contact.getLongitude().equals("Nan"));
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_form);
-        initMap();
 
         Bundle extras = getIntent().getExtras();
 
-        TextInputLayout tlAddress = findViewById(R.id.tlAddress);
-        btnAdd = findViewById(R.id.btnAddContact);
         MaterialButton btnPickPhoto = findViewById(R.id.btnPickPhoto);
-
-        tlAddress.setEndIconOnClickListener(view -> searchLocation());
 
         if (extras != null) {
             contactId = (Long) extras.get("id");
             contact = ContactDbHelper.getId(contactId);
             onEditIntent();
-            btnAdd.setOnClickListener(view -> onEditSave());
-        } else {
-            btnAdd.setOnClickListener(view -> onAddContact());
         }
 
         btnPickPhoto.setOnClickListener(view -> pickPhoto());
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mGmap = googleMap;
-        LatLng locs;
-
-        if (contact != null && locationProvided()) {
-            double lat = Double.parseDouble(contact.getLatitude());
-            double lng = Double.parseDouble(contact.getLongitude());
-
-            mapLatLng = new LatLng(lat, lng);
-            setPointOnMap(lat, lng);
-        }
-        else {
-            locs = new LatLng(DEFAULT_LAT, DEFAULT_LNG);
-            mGmap.moveCamera(CameraUpdateFactory.newLatLngZoom(locs, 2));
-        }
-    }
-
-    @Override
-    public void onMapClick(LatLng latLng) {
-        setPointOnMap(latLng.latitude, latLng.longitude);
     }
 }
